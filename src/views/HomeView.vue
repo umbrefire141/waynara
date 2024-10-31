f0037764026b7807d7970449f34b718726b4a077
Matvey <matvejaksentev@gmail.com>
2024-10-31 17:14:38 +0700

    Some details

---
+++ src/views/HomeView.vue
@@ -0,0 +1,74 @@
+<template>
+	<Header />
+	<main class="main">
+		<div class="choice">
+			<div class="container">
+				<!-- Make component typography -->
+				<div>Выбери свой активный отдых</div>
+				<div>
+					<div>
+						<label>Вид активности</label>
+						<!-- Dropdown -->
+					</div>
+					<div>
+						<label>Место</label>
+						<!-- Dropdown -->
+					</div>
+					<!-- Button -->
+				</div>
+			</div>
+		</div>
+		<div class="about">
+			<!-- Typography -->
+			<div>О нас</div>
+			<div>
+				<img src="../assets/images/logo-blue.svg" />
+				<!-- Typography -->
+				<div>Wayver – это online сообщество для людей, которые открывают для себя новые направления жизни, расширяют
+					границы своих возможностей и практикуют активный и здоровый образ жизни. Wayver поможет получить системную и
+					полезную информацию о любом спортивном событии будь то марафон, забег на 10 км, лыжные гонки или иной
+					событийный туризм. </div>
+			</div>
+		</div>
+		<div class="events">
+			<!-- Typography -->
+			<div>Топ событий месяца</div>
+			<div>
+				<div>
+					<img src="../assets/images/events/img-1.jpg" alt="">
+					<div>12 июл | СПб</div>
+					<div>OCEANMAN</div>
+				</div>
+			</div>
+		</div>
+		<div class="blog">
+			<!-- Typography -->
+			<div>Блог</div>
+			<div>Читайте статьи о событиях активного туризма от профессионалов и любителей</div>
+			<div>
+				<div>
+					<img src="../assets/images/events/img-1.jpg" alt="">
+					<img src="../assets/images/avatar.jpg" alt="" class="avatar">
+					<div>Проф велогонщик</div>
+					<div>Андрей Иванов</div>
+					<div>Самая сложная трасса года</div>
+				</div>
+			</div>
+		</div>
+	</main>
+</template>
+<script setup lang="ts">
+import Header from './components/Header/Header.vue'
+</script>
+<style scoped>
+.choice {
+	width: 100vw;
+	height: 100vh;
+	background-image: url("../assets/images/main-image-bg.jpg");
+	background-size: cover;
+	background-position: center;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+</style>
---
+++ src/views/components/Header/Header.vue
@@ -0,0 +1,86 @@
+<template>
+	<header class="header">
+		<div class="container">
+			<div class="logo">
+				<img src="@/assets/images/logo.png" />
+			</div>
+			<nav class="menu">
+				<ul class="list">
+					<li class="item active">
+						<RouterLink to="/" class="link">
+							Главная
+						</RouterLink>
+					</li>
+					<li class="item">
+						<RouterLink to="/" class="link">
+							О проекте
+						</RouterLink>
+					</li>
+					<li class="item">
+						<RouterLink to="/" class="link">
+							Войти
+						</RouterLink>
+					</li>
+					<li class="item">
+						<RouterLink to="/" class="link">
+							Регистрация
+						</RouterLink>
+					</li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue"
+export default defineComponent({
+
+})
+</script>
+<style scoped>
+.header {
+	right: 0;
+	left: 0;
+	position: absolute;
+	color: #fff;
+	padding-top: 24px;
+}
+
+.container {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	width: 100%;
+}
+
+.list {
+	display: flex;
+	align-items: center;
+	gap: 12px;
+}
+
+.item {
+	position: relative;
+	z-index: 1;
+	font-size: 1.125rem;
+	padding: 0 32px;
+}
+
+.active {
+	padding: 0.01rem 1rem;
+	font-weight: 600;
+	color: var(--color-purple);
+}
+
+.active::before {
+	content: '';
+	background-color: #fff;
+	position: absolute;
+	clip-path: polygon(0 0, 89% 0, 98% 100%, 2% 100%);
+	right: 0;
+	top: 0;
+	bottom: 0;
+	left: 0;
+	z-index: -1;
+}
+</style>
